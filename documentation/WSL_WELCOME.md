540a0048f9deb2f08d6f1d6848f5e6c7dac63956
Matthew David Andrew Clark <matthew.da.clark@outlook.com>
2023-09-12 17:20:17 -0400

    Initial Commit

---
+++ documentation/WSL_WELCOME.md
@@ -0,0 +1,7 @@
+# Visual Studio Code with Windows Subsystem for Linux - Landing Page
+
+Welcome to Windows Subsystem for Linux (WSL). Please read through this if it's your first time using this. Also, I would recommend knowing the helpful links on your WSL journey.
+
+## Getting Started
+
+More to come!
---
+++ initial_setup.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+if (( $EUID != 0 )); then
+  echo 'Run script as root'
+  exit
+fi
+
+# WSL Default Settings
+echo '[boot]' > /etc/wsl.conf
+echo 'systemd=true' >> /etc/wsl.conf
+
+# Get the system up to date
+apt-get update
+apt-get update --fix-missing
+apt full-upgrade -y
+
+# Install Ansible To Leverage That For Rest of Setup Work and main community collection for ansible
+apt install python3-pip -y && pip install ansible-core
+
+# Pull down from repository and run playbook
+mkdir /wsl
+cd /wsl
+git clone https://github.com/matthew-da-clark/wsl_image_setup.git
+cd /wsl/wsl_image_setup
+git config --global --add safe.directory '*'
+git pull
+
+# Setup Proper Config For Repo
+git config --local core.fileMode false
+
+# Install collections that are required for main setup
+ansible-galaxy collection install community.general
+ansible-galaxy collection install containers.podman
+ansible-galaxy collection install community.postgresql
+
+# Main install
+ansible-playbook init.yml --inventory inventory -vvv
